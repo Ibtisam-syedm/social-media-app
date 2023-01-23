7e633ce57c7c7f2753537bcd870bfff6bb15f523
ibtisam <m.ibtisam.syed@gmail.com>
2023-01-23 20:42:01 +0500

    Added post route and schema, functions of create, edit and delete a post

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const userRoute = require("./routes/userRoute")
 const authRoute = require("./routes/auth")
+const postRoute = require("./routes/postRoute")
+
 dotenv.config();
 
 async function connectDB (){
@@ -28,7 +30,7 @@ app.use(morgan("common"));
 
 app.use("/api/users",userRoute);
 app.use("/api/auth", authRoute);
-
+app.use("/api/posts",postRoute);
 app.listen(8000,()=>{
     console.log("server running!");
 })
